1eb59ba9b8e13bc90419d28a8f47378943984e13
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-01-12 11:19:31 -0300

    feat: Testando condições if compostas

---
+++ module_d/condicoesCompostaIF.js
@@ -0,0 +1,7 @@
+let pais = "Estados unidos";
+
+if(pais == 'Brasil') {
+    console.log("Voce é " + pais);
+} else {
+    console.log("Voce é estrangeiro")
+}
---
+++ module_d/condicoesIF.js
@@ -0,0 +1,6 @@
+let vel = 60.5;
+console.log(`À velocidade do carro é ${vel} por hora`);
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade limite permitida! Multado');
+}
+console.log('Dirija sempre com cinto de segurança');
