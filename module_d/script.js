181707ae24e6856ac0ffaacb21acc00092a2e4a6
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-01-13 23:18:15 -0300

    feat: Finalizado exercicio 1

--- module_d/script.js
+++ module_d/script.js
@@ -1,1 +1,17 @@
-alert('teste');
+function carregar() {
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('imagem');
+    let data = new Date();
+    let hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} Horas`;
+    if(hora >= 0 && hora < 12) {
+        img.src = 'manha.jpg';
+        document.body.style.backgroundColor = "yellow"
+    } else if(hora >= 12 && hora < 18) {
+        img.src = 'tarde.jpg';
+        document.body.style.backgroundColor = "orange"
+    } else {
+        img.src = 'noite.jpg';
+        document.body.style.backgroundColor = "darkblue";
+    }
+}
